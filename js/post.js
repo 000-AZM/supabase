0bb78529aa61ce6a7bfc05100d1bb5ae013f4006
000-AZM <70406553+000-AZM@users.noreply.github.com>
2025-07-17 16:09:04 +0630

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,17 +1,9 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
-const supabase = createClient('https://ozrtnqbqadnzqmyivoto.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cnRucWJxYWRuenFteWl2b3RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MzkxMTUsImV4cCI6MjA2ODMxNTExNX0.B_NYiKy0NzJrVTmIucZINas7EeBxHJzNVjLpQ7YI3Rc')
-
-
-window.uploadPost = async () => {
-  const caption = document.getElementById('caption').value
-  const file = document.getElementById('file').files[0]
-  const { data: { user } } = await supabase.auth.getUser()
-
-  const filename = `${user.id}-${Date.now()}`
-  await supabase.storage.from('images').upload(filename, file)
-  const { data: { publicUrl } } = supabase.storage.from('images').getPublicUrl(filename)
-
-  await supabase.from('posts').insert([{ user_id: user.id, caption, image_url: publicUrl }])
-  alert('Posted!')
-  window.location.href = 'index.html'
+// script/post.js
+async function createPost(content) {
+  const user = await supabase.auth.getUser();
+  const { data, error } = await supabase.from('posts').insert([
+    { user_id: user.data.user.id, content, username: user.data.user.email.split("@")[0] }
+  ]);
+  if (error) alert(error.message);
+  else window.location.href = "feed.html";
 }
